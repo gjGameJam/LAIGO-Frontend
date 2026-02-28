26f4b810d311971a544cea2cf7267f17214297b9
Grant Benson <gjbenson@ncsu.edu>
2026-02-27 23:10:24 -0500

    updated file ref to be case sensitive

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Laigo from "./Laigo.jsx";
+import Laigo from "./laigo.jsx";
 
 // Minimal render with StrictMode
 ReactDOM.createRoot(document.getElementById("root")).render(
